583e2b1b8a79f944ac2df4c11ef927560f6fb965
Wagner-Laranjeiras <wlaranjeiras.dev@gmail.com>
2025-03-20 16:56:59 +0100

    studying

--- src/components/AddIngredient.jsx
+++ src/components/AddIngredient.jsx
@@ -1,14 +1,36 @@
+
+function AddIngredientToList() {
+    ingredients.push('I was clicked.')
+    console.log(ingredients);
+}
+
 const AddIngredient = (props) => {
-    return ( 
-        <form className="flex justify-center items-center m-10">
-            <input
-                aria-label="Add ingredient"
-                type="text"
-                placeholder="e.g. Tomato"
-                className="w-3/5 max-w-5xl border-2 p-1 m-1 border-blue-900 rounded-lg shadow-sm" 
-            />
-            <button className="py-2 px-8 m-1 border-2 bg-blue-900 text-white rounded-lg text-sm">+ Add Ingredient</button>
-        </form>
+    const ingredients = ['Chicken', 'Oregano', 'Tomatoes'];
+    const ingredientsListItems = ingredients.map( (ingredient) => {
+        return <li>{ ingredient }</li>
+    })
+
+    return (
+        <>
+            <form className="flex justify-center items-center m-10">
+                <input
+                    aria-label="Add ingredient"
+                    type="text"
+                    placeholder="e.g. Tomato"
+                    className="w-3/5 max-w-5xl border-2 p-1 m-1 border-blue-900 rounded-lg shadow-sm" 
+                />
+                <button 
+                    className="py-2 px-8 m-1 border-2 bg-blue-900 text-white rounded-lg text-sm"
+                    onClick={AddIngredientToList}
+                >+ Add Ingredient</button>
+            </form>
+            <ul>
+                { ingredientsListItems }
+            </ul>
+        
+        
+        </>
+        
      );
 }
  
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,7 @@
 import chefClaudeLogo from '../images/chef-claude-icon.png';
-function clickButton() {
-    console.log('I was clicked.')
-}
-
 
 const Header = () => {
-    return ( 
+  return ( 
         <header className='flex gap-4 items-center justify-center p-6 border-b-2 border-blue-300 shadow-sm bg-blue-300'>
             <img 
                 src={chefClaudeLogo} 
@@ -15,23 +11,6 @@ const Header = () => {
             <h1 className='font-semibold text-4xl'>
                 Chef Claude
             </h1>
-
-
-
-
-
-
-
-            <img 
-                src={chefClaudeLogo} 
-                alt='chef claude logo'
-                className='w-12'
-            />
-            <button
-                onClick={clickButton}
-                className="py-2 px-8 m-1 border-2 bg-blue-900 text-white rounded-lg text-sm"
-            >Click me</button>
-
         </header>
      );
 }
