29af6dce1b83a89618884707b54d047f662dba87
Wagner-Laranjeiras <wlaranjeiras.dev@gmail.com>
2025-03-24 12:10:28 +0100

    studying

--- src/components/AddIngredient.jsx
+++ src/components/AddIngredient.jsx
@@ -30,7 +30,6 @@ const AddIngredient = (props) => {
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
-
     const [count, setCount ] = useState(0);
 
     function handleAdd() {
@@ -42,8 +41,54 @@ const AddIngredient = (props) => {
         console.log("Added.")
         setCount((prevCount) => prevCount - 1);
     }
-
     ////////////////////////////////////////////////////////////////////////////////////////////////
+    const [isGoingOut, setIsGoingOut] = useState(false);
+    let isGoingOutAnswer = isGoingOut ? "Yes" : "Nop";
+
+    function handleGoingOut() {
+        setIsGoingOut((prevIsGoingOut) => !prevIsGoingOut);
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    const favoriteThingsSong = [
+        "Raindrops on roses",
+        "Whiskers on kittens",
+        "Bright copper kettles",
+        "Warm woolen mittens",
+        "Brown paper packages tied up with strings",
+        "Cream-colored ponies",
+        "Crisp apple strudels",
+        "Doorbells",
+        "Sleigh bells",
+        "Schnitzel with noodles",
+        "Wild geese that fly with the moon on their wings",
+        "Girls in white dresses with blue satin sashes",
+        "Snowflakes that stay on my nose and eyelashes",
+        "Silver-white winters that melt into springs",
+    ]
+
+    const [myFavoriteThings, setMyFavoriteThing] = useState([]);
+
+    function addFavoriteThing() {
+        if ()
+
+        setMyFavoriteThing(
+            (prevFavoriteThing) => {
+                return [
+                    ...prevFavoriteThing,
+                    favoriteThingsSong[prevFavoriteThing.length]
+                ]
+                
+            }
+        )
+    }
+
+    const favoriteThing = myFavoriteThings.map((item) =>
+        <li key={item}>{item}</li>
+    )
+
+
+
+
 
     return (
         <>
@@ -70,19 +115,42 @@ const AddIngredient = (props) => {
 
             <div className="bg-black p-4 flex flex-col items-center justify-center">
                 <h1 className="text-white text-4xl font-bold mb-3">Count</h1>
-                <div className="flex gap-2 items-center justify-center">
+                <div className="flex items-end justify-center">
                     <button 
                         onClick={handleSubtract}
-                        className="rounded-full text-white bg-gray-600 w-16 h-16"
+                        className="z-10 rounded-full text-white bg-gray-400 w-16 h-16 text-3xl font-semibold"
                         aria-label="Decrease count"
                     >-</button>
-                    <h2 className="flex justify-center text-4xl rounded-full font-semibold content-center items-center bg-white w-26 h-26">{count}</h2>
+                    <h2 className="mx-[-16px] flex justify-center text-4xl rounded-full font-semibold content-center items-center bg-white w-26 h-26">{count}</h2>
                     <button 
                         onClick={handleAdd} 
-                        className="rounded-full bg-gray-600 text-white w-16 h-16"
+                        className="z-10 rounded-full bg-gray-400 text-white w-16 h-16 text-3xl font-semibold"
                         aria-label="Increase count"
                     >+</button>
                 </div>
+
+                <h1 className="mt-12 text-white text-4xl font-bold mb-3">Do I feel like going out tonight?</h1>
+                <button
+                    className="flex justify-center text-4xl rounded-full font-semibold content-center items-center bg-white w-26 h-26"
+                    onClick={handleGoingOut}
+                    aria-label={`The current answer is ${isGoingOutAnswer}`}
+                >{isGoingOutAnswer}</button>
+
+                <h1 className="mt-12 text-white text-4xl font-bold mb-3">My Favorite things</h1>
+                <button
+                    id="favoriteThingsButton"
+                    className="flex justify-center text-xl rounded-xl font-semibold content-center items-center bg-white p-6"
+                    onClick={addFavoriteThing}
+                >Add favorite thing</button>
+                <section
+                    aria-live="polite"
+                >
+                    <ul 
+                        className="text-white text-xl m-4 funt-semibold list-disc w-full"
+                    >
+                      {favoriteThing}  
+                    </ul>
+                </section>
             </div>
         
         
