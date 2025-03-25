4c9b34ff9d3893066f9c02f8c467fdea7f74ff50
Wagner-Laranjeiras <wlaranjeiras.dev@gmail.com>
2025-03-25 12:37:12 +0100

    move components

--- src/components/AddIngredient.jsx
+++ src/components/AddIngredient.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
 
 const AddIngredient = (props) => {
-    const [ingredients, setIngredients] = useState( [] )
-    const minimalIngredientNumber = 4;
-    const hasIngredient = ingredients.length > 0;
-    const ingredientsListItems = ingredients.map( (ingredient) => {
-        return <li key={ingredient} className="mb-4">{ ingredient }</li>
-    })
-
+    const [ingredients, setIngredients] = useState( [] );
+    
     function handleSubmit(formData) {
         const newIngredient = formData.get('ingredient');
 
@@ -19,17 +14,16 @@ const AddIngredient = (props) => {
         )
     }
 
-    function removeIngredient(e) {
-        const formData = new FormData(e.currentTarget);
-        e.preventDefault();
-        const ingredientToRemove = formData.get('ingredient');
-
-        setIngredients((prevIngredients) => 
-            prevIngredients.filter(ingredient => ingredient !== ingredientToRemove)
-        )
-    }
-
-
+    // function removeIngredient(e) {
+    //     const formData = new FormData(e.currentTarget);
+    //     e.preventDefault();
+    //     const ingredientToRemove = formData.get('ingredient');
+    //     console.log('remove ingredient');
+    //     console.log('ingredient: ', ingredientToRemove)
+    //     setIngredients((prevIngredients) => 
+    //         prevIngredients.filter(ingredient => ingredient !== ingredientToRemove)
+    //     )
+    // }
 
     return (
         <>
@@ -45,25 +39,14 @@ const AddIngredient = (props) => {
                     className="py-2 px-8 m-1 border-2 bg-blue-900 text-white rounded-lg text-sm"
                     type="submit"
                 >+ Add Ingredient</button>
-                <button 
-                    className="py-2 px-8 m-1 border-2 bg-red-400 text-white rounded-lg text-sm"
-                    onClick={removeIngredient}
-                >- Remove Ingredient</button>
+                { hasIngredient ? (
+                    <button 
+                        className="py-2 px-8 m-1 border-2 bg-red-400 text-white rounded-lg text-sm"
+                        onClick={removeIngredient}
+                        type="button"
+                    >- Remove Ingredient</button>
+                ) : null }
             </form>
-            { hasIngredient ? <p className="mx-10 text-3xl font-bold">Ingredients on hand</p> : null }
-            <ul className="flex flex-col mx-16 my-6 list-disc text-gray-600 font-semibold tracking-wide">
-                { ingredientsListItems }
-            </ul>
-            { ingredients.length > minimalIngredientNumber ? (
-                <div className="flex justify-around items-center max-w-3xl mx-auto bg-blue-100 rounded-lg p-8">
-                    <div className="flex flex-col gap-4">
-                        <span className="font-semibold text-xl">Ready for a recipe?</span>
-                        <span className="text-gray-600">Generate a recipe from your list of ingredients</span>
-                    </div>    
-                    <button className="text-xl text-white font-medium rounded-lg bg-orange-400 px-6 py-2">Get a recipe</button>
-                </div>
-            ) : null }
-
         </>
         
      );
---
+++ src/components/ListIngredients.jsx
@@ -0,0 +1,19 @@
+const ListIngredients = (props) => {
+    const hasIngredient = ingredients.length > 0;
+
+    const ingredientsListItems = ingredients.map( (ingredient) => {
+        return <li key={ingredient} className="mb-4">{ ingredient }</li>
+    })
+
+    return ( 
+        <section>
+            { hasIngredient ? <h2 className="mx-10 text-3xl font-bold">Ingredients on hand</h2> : null }
+
+            <ul className="flex flex-col mx-16 my-6 list-disc text-gray-600 font-semibold tracking-wide">
+                { ingredientsListItems }
+            </ul>
+        </section>
+     );
+}
+ 
+export default ListIngredients;
